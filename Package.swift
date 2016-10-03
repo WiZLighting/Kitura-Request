32d24e40c745e0625af046d50d2e01c9bddfd7e3
Gleb Vodovozov <vodovozovge@gmail.com>
2016-10-03 17:40:31 +0800

    corrected SwiftyJSON repo url

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "KituraRequest",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1),
-	.Package(url: "https://github.com/IBM-Swift/SwiftyJSON", majorVersion: 14),
+	.Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 14),
     ]
 )
